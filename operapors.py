9799c00f7e9c9db9eb7d5d93defa436ec96eb447
yogiy5820-sketch <yogiy5820@gmail.com>
2025-12-18 20:12:52 +0530

    Add files via upload

---
+++ operapors.py
@@ -0,0 +1,73 @@
+###
+#Short Hand Operators, Assignment Operators
+#It help us in assigning any datatype to a variabi 
+#+=Add and assign a += 3
+#-= subtract AND ASSIGN A-=3
+#= multiply and assign a=3
+#\=divide ans assign a\=3
+#\\=floor divide and assign a\\=3
+#%= moduls ans assign a%= 3
+#**=Exponential and assign a *= 2 ### 
+
+''' use to combine exeprasion 
+and =logical a and b 
+or = logical a or b 
+not = logical not a '''
+'''
+#or
+#Return False only if both the values are False.
+# If both the values are true then it will pick the left value.
+#left  | rihgt  | output  |
+ false | true   | true    |
+ true  | false  | true    |
+ true  | true   | true    |
+ false | false  | false   | ''' 
+#60 or 0
+#print (""or -13)
+#print (1 or True)
+'''
+#and 
+#Return False only if both the values are False.
+# If both the values are true then it will pick the right value.
+#left  | rihgt  | output   |
+ false | true   | false    |
+ true  | false  | false    |
+ false | false  | false    | 
+ true  | true   | true     |'''
+
+#print (0and 2)
+#print  (2and 0)
+#print  (13and 20) 
+
+#print(45 and 67 or 13 and 45 and 12)
+#print (not False) 
+#print (not True )
+
+
+
+'''area of rectangle = l*w
+l=input(" enter lenth of rectangle")
+w=input(" emter widht of rectangle")
+l=int(l)
+w=int(w)
+print(l*w)
+print(type(l*w))'''
+
+'''parameter of rectangle = 2*l*w
+l=input(" enter lenth of rectangle")
+w=input(" emter widht of rectangle")
+l=int(l)
+w=int(w)
+print(2*l*w)'''
+
+'''area of circle  is =(3.14 * radius * radius)
+radius=input("enter radius")
+radius=int(radius)
+print(3.14 * radius * radius)'''
+
+'''milk_quantity=int(input("enter number of  milk quantity  ") )
+rise_quantity=int(input("enter number of rise  quantity  ") )
+
+milk_prize=milk_quantity*20
+rise_prize=rise_quantity*10
+print("total amount" ,milk_prize+rise_prize)'''
